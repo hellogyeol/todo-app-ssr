44757161dbfd6ddd8bf7bc072ffba003e17aa7de
hellogyeol <hellogyeol@gmail.com>
2023-02-16 18:14:34 +0900

    feat: To-Do 목록 렌더링

--- app.js
+++ app.js
@@ -24,9 +24,29 @@ app.use(express.urlencoded({extended: true}));  // form으로 전달 받은 값 
 app.use(express.json());  // JSON 데이터 해석
 
 /* 라우팅 */
-
+app.get('/', (req, res) => {
+  renderToDoList(res);
+});
 
 /* 웹서버 포트 지정 */
 app.listen(port, () => {
   console.log(`To-Do app listening on port ${port}`);
 });
+
+
+/* 함수 선언 */
+
+/**
+ * To-Do 목록 조회 함수.
+ * DB에서 목록을 조회 후 클라이언트에게 전달
+ */
+async function renderToDoList(res) {
+  await client.connect();
+  const col = client.db('ssrDb').collection('ssrCol');
+  const todoList = await col.find({}).toArray();
+  console.log(todoList);
+
+  res.render('index', {
+    todoList: todoList
+  });
+}
